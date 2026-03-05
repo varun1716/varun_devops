b5146f18e7bdd8edcfdc3a9f1e9fa66daca6cbf1
P V SAI SATYA KRISHNA <148329234+varun1716@users.noreply.github.com>
2026-03-05 11:32:06 +0530

    Update CseController.java

--- src/main/java/com/example/demo/CseController.java
+++ src/main/java/com/example/demo/CseController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CseController {
-	@GetMapping("/cseadd")
-	public int addCSE(@RequestParam int a, @RequestParam int b) {
-		return a + b;
-	}
+
+    @GetMapping("/cseadd")
+    public String addCSE(@RequestParam int a, @RequestParam int b) {
+        int sum = a + b;
+        return "The value is " + sum + " and  roll number is 23MH1A05K2";
+    }
 }
